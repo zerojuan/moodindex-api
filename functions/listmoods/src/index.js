bb77cf0864d8b5c08d877fb14cb178798c32bae1
Julius Cebreros <me@juliuscebreros.com>
2017-07-01 17:54:24 +1200

    Add mood list index

---
+++ functions/listmoods/src/index.js
@@ -0,0 +1,13 @@
+import MoodIndex from 'Lib/mood';
+
+export default function( e, context ) {
+  console.log( 'MoodIndex Event', e );
+
+  return MoodIndex( e )
+    .then( ( result ) => {
+      return context.succeed( result );
+    } )
+    .catch( ( err ) => {
+      return context.fail( err );
+    } );
+}
